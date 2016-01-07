135c4d26c23e43caa72989deefaae9acab3a063f
rachelsmithcode <rachelcodesmith@outlook.com>
2016-01-07 18:23:50 +0000

    fizzbuzz made and tested

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,21 @@
+require 'fizzbuzz.rb'
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+
+  it 'returns the number when passed number that is not a multiple of 3' do
+    expect(fizzbuzz(1)).to eq 1
+  end
+
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+
+
+end
