e5fead1c75eba728cfb1976d56b5bda1d56a94e6
rachelsmithcode <rachelcodesmith@outlook.com>
2016-01-11 14:23:37 +0000

    fizzbuzz solved within class, tested at each stage via rspec.

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,6 +1,8 @@
-def fizzbuzz(number)
-    return 'fizzbuzz' if number % 3 == 0 && number % 5 == 0
-    return 'fizz' if number % 3 == 0
-    return "buzz" if number % 5 == 0
-    number
+class Fizzbuzz
+  def check_if_fb(number)
+      return 'fizzbuzz' if number % 3 == 0 && number % 5 == 0
+      return 'fizz' if number % 3 == 0
+      return "buzz" if number % 5 == 0
+      number
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,20 +1,21 @@
 require 'fizzbuzz.rb'
 
-describe 'fizzbuzz' do
+describe Fizzbuzz do
+  subject(:fizzbuzz) {described_class.new}
   it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz.check_if_fb(3)).to eq 'fizz'
   end
 
   it 'returns the number when passed number that is not a multiple of 3' do
-    expect(fizzbuzz(1)).to eq 1
+    expect(fizzbuzz.check_if_fb(1)).to eq 1
   end
 
   it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
+    expect(fizzbuzz.check_if_fb(5)).to eq 'buzz'
   end
 
   it 'returns "fizzbuzz" when passed 15' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    expect(fizzbuzz.check_if_fb(15)).to eq 'fizzbuzz'
   end
 
 
